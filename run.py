97633acd4ec0c75e8a9aa40caaecf07c0793c126
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2025-01-12 20:16:45 +0000

    connecting string to mongodb using pymongo library

--- run.py
+++ run.py
@@ -7,3 +7,9 @@ from bson import ObjectId
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Secures random secret key for sessions
 bcrypt = Bcrypt(app)
+
+# MongoDB connection string 
+client = MongoClient('mongodb+srv://Chris:Cmoreton3912@myrecipe.95tdx.mongodb.net/recipe_app?retryWrites=true&w=majority&appName=Myrecipe')
+db = client['recipe_app']
+users_collection = db['users']
+recipes_collection = db['recipes']
